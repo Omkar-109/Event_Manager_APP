cea6ddd8b201dd5aeba3f47de452fe0713cba82a
Omkar-109 <127653970+Omkar-109@users.noreply.github.com>
2024-09-21 23:44:17 +0530

    add

--- app/src/main/java/com/examples/allnewownevent/BudgetActivity.java
+++ app/src/main/java/com/examples/allnewownevent/BudgetActivity.java
@@ -3,12 +3,16 @@ package com.examples.allnewownevent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class BudgetActivity extends AppCompatActivity {
-
+    ImageButton backToHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget);
+        backToHome = findViewById(R.id.backToHome);
+
+        backToHome.setOnClickListener(view -> this.finish());
     }
 }
--- app/src/main/java/com/examples/allnewownevent/VendorsActivity.java
+++ app/src/main/java/com/examples/allnewownevent/VendorsActivity.java
@@ -3,12 +3,17 @@ package com.examples.allnewownevent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class VendorsActivity extends AppCompatActivity {
 
+    ImageButton backToHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendors);
+        backToHome = findViewById(R.id.backToHome2);
+
+        backToHome.setOnClickListener(view -> this.finish());
     }
 }
